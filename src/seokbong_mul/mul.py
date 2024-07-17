20eb0beffa022643119165982c62d3e4b07f303c
Seokxkyu <kyuseok000@gmail.com>
2024-07-17 10:13:32 +0900

    주석 처리

--- src/seokbong_mul/mul.py
+++ src/seokbong_mul/mul.py
@@ -1,10 +1,10 @@
 import sys
-
+'''
 def mul():
     first_num = int(input("first_num: "))
     second_num = int(input("second_num: "))
     print(first_num * second_num)
-
+'''
 def seokbong_mul():
     a = int(sys.argv[1])
     b = int(sys.argv[2])
